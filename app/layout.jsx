7d1d6cc298eff6b9386ff30003d294a11d3a77f2
suryapranesh4 <suryapranesh@yahoo.ca>
2023-05-17 00:04:33 -0400

    added favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,10 +6,20 @@ import Nav from "@components/Nav";
 export const metadata = {
   title: "Promptopedia",
   description: "Discover and share AI Prompts",
+  icons: {
+    icon: {
+      url: "/favicon.ico",
+      type: "image/ico",
+    },
+    shortcut: { url: "/favicon.ico", type: "image/ico" },
+  },
 };
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="../public/assets/images/logo.svg" />
+      </head>
       <body>
         <Provider>
           <div className="main">
